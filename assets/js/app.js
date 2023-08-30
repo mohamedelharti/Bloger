b7efe3e765ef36090518c8f324f39c02676b4152
mohamedelharti <mohamedharti1989@gmail.com>
2023-08-30 07:17:07 +0100

    Création du template de base avec Bootstrap 5

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,15 @@
 
+import { Dropdown } from 'bootstrap';
 import '../css/app.scss';
 
+document.addEventListener('DOMContentLoaded', ()=>{
+    enableDropdowns();
+});
 
+
+const enableDropdowns = () =>{
+    const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+          dropdownElementList.map(function (dropdownToggleEl){
+            return new Dropdown(dropdownToggleEl)
+          });
+}
